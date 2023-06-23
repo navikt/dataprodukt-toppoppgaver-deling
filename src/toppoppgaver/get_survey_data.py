49d72330a05a6b5902b19862932e516e20188956
Tobias McVey <tobias.mcvey@gmail.com>
2023-06-23 13:52:57 +0200

    method for fetching question keys and values from task analytics

---
+++ src/toppoppgaver/get_survey_data.py
@@ -0,0 +1,31 @@
+# %%
+from pathlib import Path
+
+import pandas as pd
+
+# %%
+def get_survey_questions(dataset_path:Path):
+    """
+    Get survey questions from a Task analytics survey as a dictionary
+
+    Questions are stored both in the first and second row, so questions have an key representing the question ID and a value representing the text of the question.
+
+    Parameters:
+    -----------
+    dataset_path: Path
+        Path to the dataframe
+    dataframe: pd.DataFrame
+        A dataframe to be analysed
+
+    Returns
+    -------
+    questions: a dataframe
+    """
+    df = pd.read_csv(dataset_path)
+    questions = dict(zip(df.columns, df.iloc[0]))
+    return questions
+
+
+# %%
+q = get_survey_questions(Path('../data/final/survey.csv'))
+# %%
